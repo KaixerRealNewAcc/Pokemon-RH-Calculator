e78507dca66b886c2cd889e776e0ef757fcf5a15
Kaixer <Kaixer3slot@gmail.com>
2026-06-27 14:57:21 +0200

    its not pretty but it should work

---
+++ calc/items.d.ts
@@ -0,0 +1,13 @@
+import type { Generation, TypeName, StatID, GenerationNum } from './data/interface';
+export declare const SEED_BOOSTED_STAT: {
+    [item: string]: StatID;
+};
+export declare function getItemBoostType(item: string | undefined): "Normal" | "Fighting" | "Flying" | "Poison" | "Ground" | "Rock" | "Bug" | "Ghost" | "Steel" | "Fire" | "Water" | "Grass" | "Electric" | "Psychic" | "Ice" | "Dragon" | "Dark" | "Fairy" | undefined;
+export declare function getBerryResistType(berry: string | undefined): "Normal" | "Fighting" | "Flying" | "Poison" | "Ground" | "Rock" | "Bug" | "Ghost" | "Steel" | "Fire" | "Water" | "Grass" | "Electric" | "Psychic" | "Ice" | "Dragon" | "Dark" | "Fairy" | undefined;
+export declare function getFlingPower(item?: string, gen?: GenerationNum): 0 | 100 | 20 | 10 | 50 | 40 | 80 | 60 | 30 | 130 | 85 | 110 | 95 | 90 | 70;
+export declare function getNaturalGift(gen: Generation, item: string): {
+    t: TypeName;
+    p: number;
+};
+export declare function getTechnoBlast(item: string): "Fire" | "Water" | "Electric" | "Ice" | undefined;
+export declare function getMultiAttack(item: string): TypeName | undefined;
