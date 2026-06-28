5b44a7dc7ecb507ae25c1faeb1ccda076868aea4
Kaixer <Kaixer3slot@gmail.com>
2026-06-28 02:18:36 +0200

    pt 2

---
+++ src/js/data/sets/template.js
@@ -0,0 +1,472 @@
+var SETDEX_SV = {
+    //this is a template for adding new sets, do not use this as a set.
+    "Missingno": {
+    "Template": {
+        "level": 0,
+        "ability": "",
+        "moves":[
+            "",
+            "",
+            "",
+            "",
+        ],
+        "nature":"",
+        "item":"",
+        "index": 0,
+    },
+   },
+   //End of template
+
+   "Bulbasaur": {
+   },
+   "Ivysaur": {
+   },
+   "Venusaur": {
+   },
+   "Squirtle": {
+   },
+   "Wartortle": {
+   },
+   "Blastoise": {
+   },
+   "Charmander": {
+   },
+   "Charmeleon": {
+   },
+   "Charizard": {
+   },
+   "Caterpie": {
+   },   
+   "Metapod": {
+   },
+   "Butterfree": {
+   },
+   "Weedle": {
+   },
+   "Kakuna": {
+   },
+   "Beedrill": {
+   },
+   "Pidgey": {
+   },
+   "Pidgeotto": {
+   },
+   "Pidgeot": {
+   },
+   "Rattata": {
+   },
+   "Raticate": {
+   },
+   "Spearow": {
+   },       
+   "Fearow": {
+   },
+   "Ekans": {
+   },
+   "Arbok": {
+   },
+   "Pikachu": {
+   },
+   "Raichu": {
+   },   
+   "Raichu-Alola": {
+   },
+   "Sandshrew": {
+   },
+   "Sandshrew-Alola": {
+   },
+   "Sandslash": {
+   },
+   "Sandslash-Alola": {
+   },
+   "Nidoran-F": {
+   },
+   "Nidorina": {
+   },
+   "Nidoqueen": {
+   },
+   "Nidoran-M": {
+   },
+   "Nidorino": {
+   },
+   "Nidoking": {
+   },
+   "Clefairy": {
+   },
+   "Clefable": {
+   },
+   "Vulpix": {
+   },
+   "Vulpix-Alola": {
+   },
+   "Ninetales": {
+   },
+   "Ninetales-Alola": {
+   },
+   "Jigglypuff": {
+   },
+   "Wigglytuff": {
+   },
+   "Zubat": {
+   },
+   "Golbat": {
+   },
+   "Oddish": {
+   },
+   "Gloom": {
+   },
+   "Vileplume": {
+   },
+   "Paras": {
+   },
+   "Parasect": {
+   },
+   "Venonat": {
+   },
+   "Venomoth": {
+   },
+   "Diglett": {
+   },
+   "Diglett-Alola": {
+   },
+   "Dugtrio": {
+   },
+   "Dugtrio-Alola": {
+   },
+   "Meowth": {
+   },
+   "Meowth-Alola": {
+   },
+   "Meowth-Galar": {
+   },
+   'Persian': {
+   },
+   "Persian-Alola": {
+   },
+   "Perserrker": {
+   },
+   "Psyduck": {
+   },
+   "Golduck": {
+   },
+   "Mankey": {
+   },
+   "Primeape": {
+   },
+   "Growlithe": {
+   },
+   "Growlithe-Hisui": {
+   },
+   "Arcanine": {
+   },
+   "Arcanine-Hisui": {
+   },
+   "Poliwag": {
+   },
+   "Poliwhirl": {
+   },
+   "Poliwrath": {
+   },
+   "Politoed": {
+   },
+   "Abra": {
+   },
+   "Kadabra": {
+   },
+   "Alakazam": {
+   },
+   "Machop": {
+   },
+   "Machoke": {
+   },
+   "Machamp": {
+   },   
+   "Bellsprout": {
+   },
+   "Weepinbell": {
+   },
+   "Victreebel": {
+   },
+   "Tentacool": {
+   },
+   "Tentacruel": {
+   },
+   "Geodude": {
+   },
+   "Geodude-Alola": {
+   },
+   "Graveler": {
+   },
+   "Graveler-Alola": {
+   },
+   "Golem": {
+   },
+   "Golem-Alola": {
+   },
+   "Ponyta": {
+   },
+   "Ponyta-Galar": {
+   },
+   "Rapidash": {
+   },
+   "Rapidash-Galar": {
+   },
+   "Slowpoke": {
+   },
+   "Slowpoke-Galar": {
+   },
+   "Slowbro": {
+   },
+   "Slowbro-Galar": {
+   },
+   "Slowking": {
+   },   
+   "Slowking-Galar": {
+   },
+   "Magnemite": {
+   },
+   "Magneton": {
+   },
+   "Magnezone": {
+   },
+   "Farfetch'd": {
+   },
+   "Farfetch'd-Galar": {
+   },
+   "Sirfetch'd": {
+   },
+   "Doduo": {
+   },
+   "Dodrio": {
+   },
+   "Seel": {
+   },
+   "Dewgong": {
+   },
+   "Grimer": {
+   },  
+   "Grimer-Alola": {
+   },
+   "Muk": {
+   },
+   "Muk-Alola": {
+   },
+   "Shellder": {
+   },
+   "Cloyster": {
+   },
+   "Gastly": {
+   },
+   "Haunter": {
+   },
+   "Gengar": {
+   },
+   "Gengar-Mega": {
+   },
+   "Onix": {
+   },
+   "Steelix": {
+   },
+   "Steelix-Mega": {
+   },
+   "Drowzee": {
+   },
+   "Hypno": {  
+   },
+   "Krabby": {
+   },
+   "Kingler": {
+   },
+   "Voltorb": {
+   },
+   "Voltorb-Hisui": {
+   },
+   "Electrode": {
+   },
+   "Electrode-Hisui": {
+   },
+   "Exeggcute": {
+   },
+   "Exeggutor": {
+   },   
+   "Exeggutor-Alola": {
+   },
+   "Cubone": {
+   },
+   "Marowak": {
+   },
+   "Marowak-Alola": {
+   },   
+   "Hitmonlee": {
+   },
+   "Hitmonchan": {
+   },
+   "Hitmontop": {
+   },
+   "Lickitung": {
+   },
+   "Lickilicky": {
+   },
+   "Koffing": {
+   },
+   "Weezing": {
+   },
+   "Weezing-Galar": {
+   },
+   "Rhyhorn": {
+   },
+   "Rhydon": {
+   },
+   "Rhyperior": {
+   },
+   "Chansey": {
+   },
+   "Blissey": {
+   },
+   "Tangela": {
+   },
+   "Tangrowth": {
+   },
+   "Kangaskhan": {
+   },
+   "Kangaskhan-Mega": {
+   },
+   "Horsea": {
+   },
+   "Seadra": {
+   },
+   "Kingdra": {
+   },
+   "Goldeen": {
+   },
+   "Seaking": {
+   },
+   "Staryu": {
+   },
+   "Starmie": {
+   },
+   "Mr. Mime": {
+   },
+   "Mr. Mime-Galar": {
+   },
+   "Mr. Rime": {
+   },
+   "Scyther": {
+   },
+   "Scizor": {
+   },
+   "Scizor-Mega": {
+   },
+   "Kleavor": {
+   },
+   "Jynx": {
+   },
+   "Elekid": {
+   },
+   "Electabuzz": {
+   },
+   "Electivire": {
+   },
+   "Magby": {
+   },
+   "Magmar": {
+   },
+   "Magmortar": {
+   },
+   "Pinsir": {
+   },
+   "Pinsir-Mega": {
+   },
+   "Tauros": {
+   },
+   "Tauros-Paldea": {
+   },
+   "Magikarp": {
+   },
+   "Gyarados": {
+   },
+   "Gyarados-Mega": {
+   },
+   "Lapras": {
+   },
+   "Ditto": {
+   },
+   "Eevee": {
+   },
+   "Vaporeon": {
+   },
+   "Jolteon": {
+   },
+   "Flareon": {
+   },
+   "Espeon": {
+   },
+   "Umbreon": {
+   },
+   "Leafeon": {
+   },
+   "Glaceon": {
+   },
+   "Sylveon": {
+   },   
+   "Porygon": {
+   },
+   "Porygon2": {
+   },
+   "Porygon-Z": {
+   },
+   "Omanyte": {
+   },
+   "Omastar": {
+   },
+   "Kabuto": {
+   },
+   "Kabutops": {    
+   },
+   "Aerodactyl": {
+   },
+   "Aerodactyl-Mega": {
+   },
+   "Snorlax": {
+   },
+   "Articuno": {
+   },
+   "Zapdos": {
+   },
+   "Moltres": {
+   },
+   "Dratini": {
+   },
+   "Dragonair": {
+   },
+   "Dragonite": {
+   },
+   "Mewtwo": {
+   },
+   "Mewtwo-Mega-X": {
+   },
+   "Mewtwo-Mega-Y": {
+   },   
+   "Mew": {
+   },
+   "Chikorita": {
+   },
+   "Bayleef": {
+   },
+   "Meganium": {
+   },
+   "Cyndaquil": {
+   },
+   "Quilava": {
+   },
+   "Typhlosion": {
+   },
+   "Typhlosion-Hisui": {
+   },
+   "Totodile": {
+   },
+   "Croconaw": {        
+   },
+   "Feraligatr": {
+   },
+};
